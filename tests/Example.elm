9c8bcf32d131cda98fe3cad570516f61a2720e09
Tobias Wentzlaff <tobias@wentzlaff.net>
2019-07-31 01:03:14 +0200

    Add elm-test with small example.

---
+++ tests/Example.elm
@@ -0,0 +1,26 @@
+module Example exposing (suite)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "MyExampleTest"
+        [ describe "String.reverse"
+            [ test "reverses a tuple of known strings" <|
+                \_ ->
+                    ("ABCDEFG", "XYZ")
+                        |> (String.reverse
+                        |> Expect.equal "GFEDCBA"
+
+            -- fuzz runs the test 100 times with randomly-generated inputs!
+            , fuzz string "restores the original string if you run it again" <|
+                \randomlyGeneratedString ->
+                    randomlyGeneratedString
+                        |> String.reverse
+                        |> String.reverse
+                        |> Expect.equal randomlyGeneratedString
+            ]
+        ]
